f48cac717c10bd520986085663f523b343cd8f81
Peter Cho <pcho51990@gmail.com>
2020-06-19 21:21:06 -0400

    added header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './header.styles.scss';
 
-const Header = () => <div className='header'>Header will be here.</div>;
+const Header = () => (
+  <header>
+    <span>Hit-the-Trail</span>
+    <nav>
+      <Link to='/'>MAPS</Link>
+      <Link to='/'>TRAILS</Link>
+      <Link to='/'>SAVED LISTS</Link>
+    </nav>
+  </header>
+);
 
 export default Header;
